c8f4dc7d25138c3f544210ff2bae7d4e03707069
Darius W <iamdariusw@gmail.com>
2024-01-15 18:17:03 -0500

    adding basic layout

--- hunger-app/src/App.tsx
+++ hunger-app/src/App.tsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import {ChangeEvent, useState} from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [address, setAddress] = useState("")
+
+  let buttonClick = () => {
+    console.log('Button was clicked ' + 'Count is ' +count)
+    setCount(count+1);
+  }
+ 
+  function getData(event: ChangeEvent<HTMLInputElement>){
+    setAddress(event.target.value)
+    console.log(event.target.value)
+  }
 
   return (
-    <div>
-      <h1>
-        Words
-      </h1>
-      <input />
-      <button>
-        A button
-      </button>
+    <div id='modal'>
+      <div id='content'>
+        <h1>Hunger</h1>
+        {/* <input type="text" id='address' placeholder="Zip or Address" onChange={getData} /> */}
+        <input type="submit" id='submitButton' value='submit' className='submit'/>
+      </div>
     </div>
   )
 }
