31f35dd8c56f7bfd5dc7e75c90b4267335124101
Dylan Dinh <dylandinh3@hotmail.com>
2024-01-20 14:45:07 -0500

    Added address validation api call

--- hunger-app/src/App.tsx
+++ hunger-app/src/App.tsx
@@ -1,60 +1,88 @@
-import { ChangeEvent, useState, FormEvent , useEffect} from "react";
+import { ChangeEvent, useState, FormEvent, useEffect } from "react";
 
 function App() {
-  const [address, setAddress] = useState("");
+  //const [address, setAddress] = useState("");
   const [submittedAddress, setSubmittedAddress] = useState("");
-  const [advice, setAdvice] = useState("")
+  const [fetchAddress, setFetchAddress] = useState("");
+  const [advice, setAdvice] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  const apiKey = "AIzaSyABISqSmSD1EjzYxxSmWQqEMY6gENLjYdA";
+
   //calling an api for advice (just a test)
-  useEffect(
-    () => {
-      const fetchData = async () =>
-      { 
-      const result = await fetch("https://api.adviceslip.com/advice", {cache: "no-cache"})
-      const data = result.json().then(json => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://api.adviceslip.com/advice", {
+        cache: "no-cache",
+      });
+      const data = result.json().then((json) => {
         // console.log(json.slip.advice)
         setAdvice(json.slip.advice);
       });
-      data
-    }
+      data;
+    };
     fetchData();
-  },[]);
+  }, []);
 
   //updates address
-  function getAddress(event: ChangeEvent<HTMLInputElement>) {
-    setAddress(event.currentTarget.value);
+  function updateAddress(event: ChangeEvent<HTMLInputElement>) {
+    setSubmittedAddress(event.currentTarget.value);
   }
 
   //used on submission, prevents page refresh and collects current state of input onSubmit
-  function submitAddress(event: FormEvent<HTMLFormElement>) {
+  async function submitAddress(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log(address);
-    setSubmittedAddress(address);
-    setAddress("");
+    console.log(submittedAddress);
+    //setSubmittedAddress(address);
+    // setAddress("");
+
+    fetch(
+      "https://addressvalidation.googleapis.com/v1:validateAddress?key=" +
+        apiKey,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ address: { addressLines: [submittedAddress] } }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data),
+          setFetchAddress(data.result.address.formattedAddress),
+          setLatitude(data.result.geocode.location.latitude),
+          setLongitude(data.result.geocode.location.longitude);
+      });
+
+    setSubmittedAddress("");
   }
-  
 
   return (
-      <div className='card card-compact w-1/2 center' id="content">
-        <h1 className='card-title justify-center'>Hunger</h1>
-        <p className='card-body text-center'>{submittedAddress}</p>
-        <p className='card-body text-center font-bold'>{advice}</p>
-        <form className='card-body' onSubmit={submitAddress}>
-          <input
-            className='input outline'
-            type="text"
-            id="address"
-            value={address}
-            placeholder="Zip or Address"
-            onChange={getAddress}
-          />
-          <input
-            type="submit"
-            id="submitButton"
-            value="submit"
-            className='btn btn-secondary' 
-           />
-        </form>
-      </div>
+    <div className="card card-compact w-1/2 center" id="content">
+      <h1 className="card-title justify-center">Hunger</h1>
+      <p className="card-body text-center">
+        Your formatted address: {fetchAddress}
+      </p>
+      <p className="card-body text-center">Your latitude: {latitude}</p>
+      <p className="card-body text-center">Your longitude: {longitude}</p>
+      <p className="card-body text-center font-bold">{advice}</p>
+      <form className="card-body" onSubmit={submitAddress}>
+        <input
+          className="input outline"
+          type="text"
+          id="address"
+          value={submittedAddress}
+          placeholder="Zip or Address"
+          onChange={updateAddress}
+        />
+        <input
+          type="submit"
+          id="submitButton"
+          value="submit"
+          className="btn btn-secondary"
+        />
+      </form>
+    </div>
   );
 }
 
