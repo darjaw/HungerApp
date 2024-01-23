df0456172bdd0857315e61e2a1a0f5150539b1a2
Darius W <iamdariusw@gmail.com>
2024-01-22 19:46:33 -0500

    refactor: swapping margin for padding in most cases

--- hunger-app/src/App.tsx
+++ hunger-app/src/App.tsx
@@ -50,49 +50,51 @@ function App() {
   }
 
   return (
-    <div
-      className="card card-compact mt-28 max-w-screen-md w-3/5 m-auto border border-solid border-secondary"
-      id="content"
-    >
-      <h1 className="card-title rounded-tl-2xl font-bold font-rethinkSans text-[#1d202d] text-5xl p-5 rounded-tr-2xl justify-center bg-[#eca700]">
-        Hunger
-      </h1>
-      <p className="pb-4 text-center">
-        Your formatted address: {returnedFormattedAddress}
-      </p>
-      <p className="p-0 text-center">Your latitude: {returnedLatitude}</p>
-      <p className="p-0 text-center">Your longitude: {returnedLongitude}</p>
-      <label
-        className=" self-center text-xl input mt-10 max-md:text-sm"
-        htmlFor="address"
+    <main className="flex justify-center flex-wrap pt-28" id="container">
+      <div
+        className="card card-compact max-w-screen-md w-3/5 flex  border border-solid border-secondary"
+        id="content"
       >
-        5-digit ZIP Code or address
-      </label>
-      <input
-        className="self-center text-sm place-content-center input w-2/3 outline mb-8 max-md:text-xs"
-        type="text"
-        id="address"
-        ref={inputRef}
-        value={inputAddress}
-        placeholder="e.g. '21401' or '145 Broadway, Buffalo, NY 14203'"
-        onChange={handleInputUpdate}
-        autoComplete="off"
-      />
-      <button
-        className="btn no-animation place-self-end bg-[#e92727] hover:bg-[#9c1a1a] active:bg-[#4f0d0d] transition-all rounded-bl-2xl rounded-br-2xl rounded-tl-none rounded-tr-none w-full"
-        type="button"
-        id="testButton"
-        onClick={() => {
-          if (inputRef.current?.value !== "") {
-            handleAddressSubmission();
-          } else {
-            window.alert("Please enter a ZIP code or address");
-          }
-        }}
-      >
-        Submit
-      </button>
-    </div>
+        <h1 className="card-title rounded-tl-2xl font-bold font-rethinkSans text-[#1d202d] text-5xl p-5 rounded-tr-2xl justify-center bg-[#eca700]">
+          Hunger
+        </h1>
+        <p className="pb-4 text-center">
+          Your formatted address: {returnedFormattedAddress}
+        </p>
+        <p className="p-0 text-center">Your latitude: {returnedLatitude}</p>
+        <p className="p-0 text-center">Your longitude: {returnedLongitude}</p>
+        <label
+          className=" self-center text-xl input mt-10 max-md:text-sm"
+          htmlFor="address"
+        >
+          5-digit ZIP Code or address
+        </label>
+        <input
+          className="self-center text-sm place-content-center input w-2/3 outline mb-8 max-md:text-xs"
+          type="text"
+          id="address"
+          ref={inputRef}
+          value={inputAddress}
+          placeholder="e.g. '21401' or '145 Broadway, Buffalo, NY 14203'"
+          onChange={handleInputUpdate}
+          autoComplete="off"
+        />
+        <button
+          className="btn no-animation place-self-end bg-[#e92727] hover:bg-[#9c1a1a] active:bg-[#4f0d0d] transition-all rounded-bl-2xl rounded-br-2xl rounded-tl-none rounded-tr-none w-full"
+          type="button"
+          id="testButton"
+          onClick={() => {
+            if (inputRef.current?.value !== "") {
+              handleAddressSubmission();
+            } else {
+              window.alert("Please enter a ZIP code or address");
+            }
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </main>
   );
 }
 
